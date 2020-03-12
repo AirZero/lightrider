1e0ee7f72f2ad910a2c2a5558a38f98dcb650efc
airzero <airzero@gmail.com>
2020-03-12 19:00:39 +0200

    corona

---
+++ flamegrow.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+i=0
+
+while [ $i -gt 10 ]
+do
+	V=$(((($RANDOM) % 100) - 80))
+	echo $i
+	./all.py 0 25 $i $V 0
+	((i+=10))
+done
+./all.py 0 25 0 0 0 
---
+++ summeri.sh
@@ -0,0 +1,9 @@
+while [ 1==1 ]
+
+do
+	./flameshrink.sh
+	sleep 0.05
+	./all.py 0 25 255 0 0
+
+
+done
